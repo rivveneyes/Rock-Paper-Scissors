cd481c29a8de3a5137de4212e0ffdabea48a25b2
rivveneyes <garciajoshua907@yahoo.com>
2021-06-07 00:25:02 -0700

    reworked entire css layout also logic for app reworke.

--- scripts.js
+++ scripts.js
@@ -1,97 +1,60 @@
-const imgContainer=document.querySelector('.img-container');
-const userpicked= document.querySelector("h3");
-const pcPicked=document.querySelectorAll("h3")[1];
-const images =imgContainer.querySelectorAll('img');
-const startButton = document.querySelector("button");
-let totalWins=0;
-
-
-imgContainer.addEventListener("click", e=>{
-    let option =e.target;
-    let dupli = option.cloneNode(true)
-    let imgPicks = userpicked.childNodes;
-
-    if(option.className=="option"&& imgPicks.length<=2){
-    userpicked.appendChild(dupli);
-    userpicked.replaceChild(dupli,imgPicks[1])
-}
-})
-
-startButton.addEventListener("click", e=>{
-
-if(userpicked.childNodes.length>1){
-let scoreDisplay = document.querySelector("h2");
-let ptag = document.createElement("P");
-let pcDisplay= document.querySelector(".computer");
-let randomNum = (Math.floor(Math.random()*3));
-let pcPick =images[randomNum].cloneNode(true);
-let userPick =userpicked.childNodes[1];
-let declaration=checkWin(userPick.id ,pcPick.id);
-
-
-pcDisplay.appendChild(pcPick);
-pcDisplay.replaceChild(pcPick, pcDisplay.childNodes[1]);
-ptag.innerText=declaration;
-
-scoreDisplay.innerHTML=`YOUR WINS: ${totalWins}`
-scoreDisplay.appendChild(ptag);
-scoreDisplay.replaceChild(ptag, scoreDisplay.childNodes[1])
-winner(declaration)
-
-
-}
-else{
-    alert('please pick a option')
-}
-})
-
-function checkWin(user,pc){
-
-    if (user === pc){
-        
-   return "its a tie!";
-        
-    }
-   
-    if(user==="rock"){
-       if( pc==="paper"){return "You Lose"}
-        
-       else if(pc ==="scissors"){
-        totalWins++ 
-        return "You Win"; 
-       };
-   }
-   
-   if(user==="paper"){
-       if( pc==="scissors"){return "You Lose"}
-   
-       else if(pc ==="rock"){
-        totalWins++   
-        return "You Win";
-        };
-   }
-   
-   if(user==='scissors'){
-     if(pc ==="rock"){return "You Lose"}
-   
-       else if( pc==="paper"){
-        totalWins++   
-        return "You Win";
-        }}
-}
-
-function winner(answer){
-
-    if(answer =="You Win"){
-        userpicked.style.borderColor="blue";
-        pcPicked.style.borderColor="black";        
-     }
-      else if(answer =="You Lose"){
-        userpicked.style.borderColor="black";
-        pcPicked.style.borderColor="blue";
-      }
-    else {
-        userpicked.style.borderColor="black";
-        pcPicked.style.borderColor="black";        
+const options = document.querySelectorAll(".option");
+const personChoice = document.getElementById("person-pick");
+const pcChoice = document.getElementById("pc-picked");
+const playButton = document.getElementById("play");
+const winsDisplay = document.getElementById("total-wins");
+const outcomeDesc = document.getElementById("outcome");
+const match = {
+  wins: 0,
+  lossed: 0,
+  tie: 0,
+  person: null,
+  pc: null,
+};
+function checkWhoWon() {
+  if (
+    (match.person === "scissor" && match.pc === "paper") ||
+    (match.person === "rock" && match.pc === "scissors") ||
+    (match.person === "paper" && match.pc === "rock")
+  ) {
+    personChoice.className = "winner";
+    pcChoice.className = "loser";
+    match.wins++;
+    outcomeDesc.innerText = "You win";
+  } else if (match.person === match.pc) {
+    match.tie++;
+    personChoice.className = "tie";
+    pcChoice.className = "tie";
+    outcomeDesc.innerText = "It's a Tie";
+    return;
+  } else {
+    pcChoice.className = "winner";
+    personChoice.className = "loser";
+    match.lossed++;
+    outcomeDesc.innerText = "You Lost";
   }
+  winsDisplay.innerText = `Wins:${match.wins} Loss:${match.lossed}`;
 }
+options.forEach((option) => {
+  option.addEventListener("click", () => {
+    const html = `<figcaption>You Picked</figcaption>`;
+    let coppy = option.cloneNode();
+    personChoice.innerHTML = html;
+    personChoice.appendChild(coppy);
+    match.person = coppy.id;
+  });
+});
+
+playButton.addEventListener("click", () => {
+  if (match.person === null) {
+    outcomeDesc.innerText = "PLEASE PICK A OPTION";
+    return;
+  }
+  const html = `<figcaption>PC Picked</figcaption>`;
+  const pcPick = Math.floor(Math.random() * 3);
+  let coppy = options[pcPick].cloneNode();
+  pcChoice.innerHTML = html;
+  pcChoice.appendChild(coppy);
+  match.pc = coppy.id;
+  checkWhoWon();
+});
